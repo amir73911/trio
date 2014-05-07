ab19f6b8cd96e27543e40c048bb212437747543f
amir73911 <gafurovamir@gmail.com>
2014-05-07 21:38:57 +0400

    draggable elements + rhomb grid

--- js/js.js
+++ js/js.js
@@ -2,10 +2,21 @@ $(function(){
 
     initGrid();
 
-    $( ".test" ).draggable({ grid: [ 225, 225 ] });
-
 });
 
 function initGrid() {
-
+    var grid_size = 320/2;
+    $( ".draggable" )
+        .prepend('<div class="handler"></div>')
+        .draggable({
+            grid: [ grid_size, grid_size ],
+            //containment: ".window",
+            cursor: "move",
+            handle: ".handler",
+            start: function() {
+                $(this).removeClass(function (index, css) {
+                    return (css.match (/\bpos-\S+/g) || []).join(' ');
+                });
+            }
+        });
 }
